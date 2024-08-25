f6f6ec38e8508729a68ad89ea65a06aa089e2521
MEKUS27 <vorji325@gmail.com>
2024-08-25 13:15:30 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,50 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    // loop: true,
-
-    // Enable autoplay
-    autoplay: {
-        delay: 3000, // Delay between slides in milliseconds (3 seconds)
-        disableOnInteraction: false, // Autoplay will not stop after user interactions like swiping or clicking arrows
-    },
-
-    // If we need pagination
+var swiper = new Swiper('.swiper', {
+    // Enable pagination
     pagination: {
         el: '.swiper-pagination',
+        clickable: true, // Makes the pagination bullets clickable
     },
-
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-
-    // And if we need scrollbar
+    // Optional parameters
+    loop: true, // Enable looping
     scrollbar: {
         el: '.swiper-scrollbar',
     },
 });
+
+
+var countDownDate = new Date("oct 11, 2024 00:00:00").getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.querySelectorAll('.days').forEach((day) =>{
+        day.innerHTML = days;
+    })
+    document.querySelectorAll('.hours').forEach((hour) =>{
+        hour.innerHTML = hours;
+    })
+    document.querySelectorAll('.minutes').forEach((minute) =>{
+        minute.innerHTML = minutes;
+    })
+    document.querySelectorAll('.seconds').forEach((second) =>{
+        second.innerHTML = seconds;
+    })
+    document.querySelectorAll('.textt').forEach((text) =>{
+        text.innerHTML = textt;
+    })
+
+}, 1000)
+// Ends Here
+document.querySelectorAll('.days').forEach((day) =>{
+    console.log(day)
+})
